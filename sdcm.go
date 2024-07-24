bdb8293ddb512ad984ac710709b2a6e72c10851b
Hauke Bartsch <HaukeBartsch@gmail.com>
2024-07-24 10:08:30 +0200

    add a debug option that lists the file that are refused because of extension or content

--- sdcm.go
+++ sdcm.go
@@ -61,6 +61,7 @@ var (
 	verboseFlag      bool
 	versionFlag      bool
 	outputFolderFlag string
+	debugFlag        bool
 )
 
 func UpdateCounter(counters *sync.Map, key string) {
@@ -234,7 +235,9 @@ func walkFunc(path string, info os.FileInfo, err error) error {
 	if filepath.Ext(path) != "" {
 		if strings.ToLower(filepath.Ext(path)) != ".dcm" && !isNum(filepath.Ext(path)[1:]) && len(filepath.Ext(path)) < 5 {
 			atomic.AddInt32(&counterError, 1)
-			//fmt.Printf("ignore file: %s\n", path)
+			if debugFlag {
+				fmt.Printf("ignore file due to file extension: \"%s\"\n", path)
+			}
 			return nil // ignore this file
 		}
 	}
@@ -463,7 +466,7 @@ func walkFunc(path string, info os.FileInfo, err error) error {
 					}
 				} else {
 					// instead of copy we assume we want a symbolic link
-					exitGracefully(errors.New("unknown option for method, we support only \"copy\" and \"link\""))
+					exitGracefully(fmt.Errorf("unknown option \"%s\" for method flag, we support only \"copy\" (default) and \"link\"", methodFlag))
 				}
 				if err != nil {
 					fmt.Println(err)
@@ -476,7 +479,9 @@ func walkFunc(path string, info os.FileInfo, err error) error {
 			}
 		}
 	} else {
-		//fmt.Printf("Tried to read but failed %s\n", path)
+		if debugFlag {
+			fmt.Printf("ignore file, cannot read as DICOM: \"%s\"\n", path)
+		}
 		atomic.AddInt32(&counterError, 1)
 	}
 
@@ -576,8 +581,13 @@ func main() {
 	flag.StringVar(&methodFlag, "method", "copy", "Create symbolic links (faster) or copy files [copy|link]")
 	flag.StringVar(&outputFolderFlag, "folder", "{PatientID}_{PatientName}/{StudyDate}_{StudyTime}_{StudyInstanceUID}/{SeriesNumber}_{SeriesDescription}_{SeriesInstanceUID}/{Modality}_{SOPInstanceUID}.dcm", "Specify the requested output folder structure using the following DICOM tags:\n\t{counter}, {PatientID}, {PatientName}, {StudyDate},\n\t{StudyTime}, {SeriesDescription}, {SeriesNumber}, {StudyDescription},\n\t{Modality}, {StudyInstanceUID}, {SeriesInstanceUID}, {SOPInstanceUID}\n")
 	flag.BoolVar(&verboseFlag, "verbose", false, "Print more verbose output")
+	flag.BoolVar(&debugFlag, "debug", false, "Print out even more")
 	flag.BoolVar(&versionFlag, "version", false, "Print the version number")
 	flag.Parse()
+
+	if debugFlag {
+		verboseFlag = true
+	}
 
 	if versionFlag {
 		timeThen := time.Now()
